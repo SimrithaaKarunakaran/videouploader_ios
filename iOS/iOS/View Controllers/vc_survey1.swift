db01514020dcd9f897ba0a393982976dafefc458
haik <haik.kalantarian@gmail.com>
2018-05-08 15:11:30 -0700

    progress

--- iOS/iOS/View Controllers/vc_survey1.swift
+++ iOS/iOS/View Controllers/vc_survey1.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class vc_survey1: UIViewController {
 
+    @IBOutlet weak var LatinoSwitch: UISwitch!
+    @IBOutlet weak var AsianSwitch: UISwitch!
+    @IBOutlet weak var NativeAmericanSwitch: UISwitch!
+    @IBOutlet weak var SoutheastSwitch: UISwitch!
+    @IBOutlet weak var WhiteSwitch: UISwitch!
+    @IBOutlet weak var CaribbeanSwitch: UISwitch!
+    @IBOutlet weak var ArabSwitch: UISwitch!
+    @IBOutlet weak var PacificSwitch: UISwitch!
+    @IBOutlet weak var SouthAsianSwitch: UISwitch!
+    @IBOutlet weak var OtherSwitch: UISwitch!
+    
+    
+    
+    @IBOutlet weak var textName: UITextField!
+    @IBOutlet weak var textGender: UITextField!
+    @IBOutlet weak var textDOB: UITextField!
+    
+    @IBOutlet weak var textCountry: UITextField!
+    @IBOutlet weak var textState: UITextField!
+    @IBOutlet weak var textZIP: UITextField!
+    
+    @IBOutlet weak var textOtherDiagnosis: UITextField!
+    @IBOutlet weak var textAutism: UITextField!
+    @IBOutlet weak var textCity: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
